7a2d8dd400e191fb1796ffb4f3937139986a38af
hyoshida <hyoshida@git.com>
2021-06-22 11:23:40 +0900

    finish task 1

--- App.tsx
+++ App.tsx
@@ -1,16 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class Counter extends React.Component {
+  state = { count: 0 };
+
+  setCount = () => this.setState(
+    prevState => ({ ...prevState, count: this.state.count + 1 })
+  )
+
+  render() {
+    const { count } = this.state;
+    return (
+      <View style={[styles.bg]}>
+        <View style={{ height: 100 }}>
+          <Text style={count < 5 ? styles.less : styles.greater}>{count}回タップした。</Text>
+        </View>
+        <View style={{ height: 100 }}>
+          <TouchableOpacity style={styles.button} onPress={this.setCount}>
+            <Text style={styles.buttonText}>Click</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 }
 
+export default function App() {
+  return (<Counter />);
+}
+
+const styles = StyleSheet.create({
+  bg: { flex:1, paddingTop: 150, alignItems: 'center', backgroundColor: '#cbf35c' },
+  less: { fontSize: 25, color: '#4d3398', fontWeight: 'bold' },
+  greater: { fontSize: 25, color: '#f3845c', fontWeight: 'bold' },
+  button: {
+    width: 150,
+    height: 50,
+    alignItems: 'center',
+    paddingTop: 10,
+    borderRadius: 10,
+    backgroundColor: '#3498db'
+  },
+  buttonText: {
+    fontSize: 25,
+    color: '#fff'
+  }
+});
+
+/**
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -19,3 +57,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+*/ 
